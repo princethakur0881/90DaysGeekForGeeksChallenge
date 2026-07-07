6758275404c9fe31ced4e5f64ed49fe8a09e25d1
princethakur0881 <140616987+princethakur0881@users.noreply.github.com>
2026-07-07 16:41:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Java Switch Case statement/java-switch-case-statement.java
@@ -0,0 +1,24 @@
+class Solution {
+    static double switchCase(int choice, List<Double> arr) {
+        // code here
+        double area = 0.0;
+        switch(choice){
+            case 1:
+                double R = arr.get(0);
+               area = Math.PI*R*R;
+                break;
+              
+                case 2:
+                    double L = arr.get(0);
+                    double B =arr.get(1);
+                   area  = L*B;
+                   break;
+                
+    
+        default :
+        area = 0.0;
+        break;
+        }
+        return area;
+    }
+}
