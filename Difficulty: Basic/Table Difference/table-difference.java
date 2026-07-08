1eb32eeae048f8802710bbf39191afa6dc9f9396
princethakur0881 <140616987+princethakur0881@users.noreply.github.com>
2026-07-08 12:19:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Table Difference/table-difference.java
@@ -0,0 +1,10 @@
+class Solution {
+    public static void difference(int n1, int n2) {
+
+        // Write your code here
+        for(int i=1;i<=10;i++){
+            int diff =n1-n2;
+            System.out.print(diff*i+" ");
+        }
+    }
+}
