6553c90704232b29beff8b36ea0774fa8b7b466b
princethakur0881 <140616987+princethakur0881@users.noreply.github.com>
2026-07-08 12:33:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/The Else if Statement/the-else-if-statement.java
@@ -0,0 +1,16 @@
+class Solution {
+    public static void utility(int number) {
+        // Write the if, else if, else statements below
+        if(number>100){
+            System.out.println("Big");
+            
+        }
+        else if(number<=100&&number>=10){
+            System.out.println("Number");
+            
+        }
+        else{
+            System.out.println("Small");
+        }
+    }
+}
