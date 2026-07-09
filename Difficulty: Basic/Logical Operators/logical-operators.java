1af374fd8695f7375af6298a298d7085ceaf7755
princethakur0881 <140616987+princethakur0881@users.noreply.github.com>
2026-07-09 14:40:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Logical Operators/logical-operators.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String booleanOperations(boolean a, boolean b) {
+        // Code here
+       boolean c=a&&b;
+        boolean d=a||b;
+        boolean e=!a;
+        return c+" "+d+" "+e;
+    }
+}
