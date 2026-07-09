5cc82b5371a7ad166d3d138eb724d7d0657202a7
princethakur0881 <140616987+princethakur0881@users.noreply.github.com>
2026-07-09 15:04:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Java Inheritance/java-inheritance.java
@@ -0,0 +1,9 @@
+class cls2 extends cls1 {
+    void mul(int p, int q) { System.out.println(p * q); }
+
+    void task(int p, int q) {
+      int a = p*p;
+      int b = q*q;
+      System.out.println(a+b);
+    }
+}
