de11c817b133c6e4f16402b49decf28fde4c55c0
princethakur0881 <140616987+princethakur0881@users.noreply.github.com>
2026-07-09 15:28:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Java Basic Data Types/java-basic-data-types.java
@@ -0,0 +1,22 @@
+class Solution {
+
+    int javaIntType(Scanner sc) {
+        // code here
+        int n =sc.nextInt();
+        return n;
+    }
+
+    String javaStringType(Scanner sc) {
+        
+        // code here
+        String str = sc.next();
+        return str;
+    }
+
+        
+    float javaFloatType(Scanner sc) {
+        // code here
+        float num = sc.nextFloat();
+        return num;
+    }
+};
