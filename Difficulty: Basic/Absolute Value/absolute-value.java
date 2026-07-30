aa94a0a38e1d5dff58ad52fd554c0ffdabcdb716
princethakur0881 <140616987+princethakur0881@users.noreply.github.com>
2026-07-30 23:14:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Absolute Value/absolute-value.java
@@ -0,0 +1,16 @@
+// class Solution {
+//     public int absolute(int n) {
+//         // code here
+        
+//     }
+// }
+class Solution {
+    public int absolute(int I) {
+        // code here
+        
+        if(I<0){
+            return I*-1;
+        }else
+            return I;
+    }
+}
