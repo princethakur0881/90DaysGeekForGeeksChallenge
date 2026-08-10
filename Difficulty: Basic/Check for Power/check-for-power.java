e9ca5b3170a84be58a9f255d871263477be95c0b
princethakur0881 <140616987+princethakur0881@users.noreply.github.com>
2026-08-10 23:32:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Check for Power/check-for-power.java
@@ -0,0 +1,11 @@
+class Solution {
+    static boolean isPower(int x, int y) {
+        if (x == 1)
+            return y == 1;
+        while (y % x == 0) {
+            y = y/x;
+        }
+
+        return y == 1;
+    }
+}
