bb769fe29eb95b10292936a561fff8a5f92aff80
princethakur0881 <140616987+princethakur0881@users.noreply.github.com>
2026-08-25 16:05:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sum of Natural Numbers/sum-of-natural-numbers.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class GFG {
+    public static int Sum(int n){
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+
+        return n + Sum(n-1);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+    System.out.println(Sum(n));
+    }
+}
