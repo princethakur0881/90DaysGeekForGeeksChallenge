0105d1f600b9e6cd3d9f76d750f0e00ffdf6b960
princethakur0881 <140616987+princethakur0881@users.noreply.github.com>
2026-08-30 21:36:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Marks from Ranks/marks-from-ranks.java
@@ -0,0 +1,17 @@
+class Solution {
+    public ArrayList<Integer> getMarks(int[] l, int[] r, int[] rank) {
+        // code here
+        ArrayList<Integer> res = new ArrayList<>(); // result
+        ArrayList<Integer> list = new ArrayList<>(); 
+        for(int i=0; i<l.length; i++) {
+            int st = l[i], en = r[i];
+            for(int j=st; j<=en; j++) {
+                list.add(j);
+            }
+        }
+        for(int i=0; i<rank.length; i++) {
+            res.add(list.get(rank[i]-1));
+        }
+        return res;
+    }
+}
