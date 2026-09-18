fd50e074fc1ed8dee7faf0a4f8b9910810704dd8
princethakur0881 <140616987+princethakur0881@users.noreply.github.com>
2026-09-18 07:02:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Length of Circular Linked List/length-of-circular-linked-list.java
@@ -0,0 +1,29 @@
+/* Linked List Node Structure
+class Node
+{
+    int data;
+    Node next;
+    Node(int data)
+    {
+        this.data = data;
+        this.next = null;
+    }
+}
+*/
+class Solution {
+    public static int getLength(Node head) {
+        if(head==null) return 0;
+        
+    
+       int count = 1;
+       
+        Node currNode = head;
+        Node tempNode = head;
+        while(currNode.next !=head){
+         count++;
+            currNode  = currNode.next;
+        }
+        return count;
+        
+    }
+}
